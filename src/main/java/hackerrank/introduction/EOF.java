f654ca51761efca6c730c13e5549afe648c024ce
Tsvetoslav Tsvetkov <tsvetoslav.tsvetkov@comptel.com>
2017-12-18 14:50:13 +0200

    Passed Java End-of-file

---
+++ src/main/java/hackerrank/introduction/EOF.java
@@ -0,0 +1,20 @@
+package hackerrank.introduction;
+
+import java.util.Scanner;
+
+/**
+ * https://www.hackerrank.com/challenges/java-end-of-file/problem
+ * @author cpt2tts
+ *
+ */
+public class EOF {
+	public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int lineNumber = 1;
+        while (scanner.hasNext()) {
+			System.out.println(lineNumber + " " + scanner.nextLine());
+			lineNumber++;
+		}
+		/* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+    }
+}
