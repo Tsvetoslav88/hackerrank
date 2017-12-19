e493bf5b707fd4549444636714371ab5533a456a
Tsvetoslav Tsvetkov <tsvetoslav.tsvetkov@comptel.com>
2017-12-19 11:43:02 +0200

    Added exercise intToString

--- src/main/java/hackerrank/introduction/IntToString.java
+++ src/main/java/hackerrank/introduction/IntToString.java
@@ -1,5 +1,23 @@
 package hackerrank.introduction;
 
-public class IntToString {
+import java.util.Scanner;
 
+public class IntToString {
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		int n = in.nextInt();
+		in.close();
+		
+		if(n > -100 && n <= 100){
+			String s = String.valueOf(n);
+			if (n == Integer.parseInt(s)) {
+				System.out.println("Good job");
+			} else {
+				System.out.println("Wrong answer.");
+			}
+		}else{
+			System.out.println("The input parameter has to be in the range between -100 and 100");
+		}
+
+	}
 }
