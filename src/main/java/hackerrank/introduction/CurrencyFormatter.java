7d8502f72ece7e2fbe2920fc6ebf3b0003121296
Tsvetoslav Tsvetkov <tsvetoslav.tsvetkov@comptel.com>
2017-12-20 17:24:10 +0200

    Made CurrencyFormatter challenge

---
+++ src/main/java/hackerrank/introduction/CurrencyFormatter.java
@@ -0,0 +1,33 @@
+package hackerrank.introduction;
+
+import java.text.NumberFormat;
+import java.util.Locale;
+import java.util.Scanner;
+
+/**
+ * https://www.hackerrank.com/challenges/java-currency-formatter/problem
+ * @author cpt2tts
+ *
+ */
+public class CurrencyFormatter {
+	public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        double payment = scanner.nextDouble();
+        scanner.close();
+
+        /* Create custom Locale for India. 
+          I used the "IANA Language Subtag Registry" to find India's country code */
+        Locale indian = new Locale("en", "IN");
+        
+        NumberFormat us = NumberFormat.getCurrencyInstance(Locale.US);
+        NumberFormat india = NumberFormat.getCurrencyInstance(indian);
+        NumberFormat china = NumberFormat.getCurrencyInstance(Locale.CHINA);
+        NumberFormat france = NumberFormat.getCurrencyInstance(Locale.FRANCE);
+        
+        /* Print output */ 
+        System.out.println("US: " + us.format(payment));
+        System.out.println("India: " + india.format(payment));
+        System.out.println("China: " + china.format(payment));
+        System.out.println("France: " + france.format(payment));
+    }
+}
