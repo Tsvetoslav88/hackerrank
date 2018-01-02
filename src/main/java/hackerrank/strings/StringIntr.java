6207ca1bdabefd6981e3c0f8e87f08d6a0f4566a
Tsvetoslav Tsvetkov <tsvetoslav.tsvetkov@comptel.com>
2018-01-02 16:59:47 +0200

    Solved StringItro challenge

--- src/main/java/hackerrank/strings/StringIntr.java
+++ src/main/java/hackerrank/strings/StringIntr.java
@@ -16,9 +16,13 @@ public class StringIntr {
 		String A = sc.next();
 		String B = sc.next();
 
-		int aLenght = A.length();
-		int bLenght = B.length();
-
-		System.out.println(aLenght + bLenght);
+		//sum the lengths of A and B 
+		System.out.println(A.length() + B.length());
+		
+		//write Yes if A is lexicographically larger than B or No if it is not
+		System.out.println(A.compareTo(B)>0?"Yes":"No");
+		
+		//capitalize the first letter in both A and B and print them on a single line, separated by a space.
+		System.out.println(A.substring(0, 1).toUpperCase() + A.substring(1) + " " + B.substring(0, 1).toUpperCase() + B.substring(1));
 	}
 }
