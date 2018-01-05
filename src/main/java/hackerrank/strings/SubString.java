f689b0eb948e2edd93350eeaff9a575645c04c06
Tsvetoslav Tsvetkov <tsvetoslav.tsvetkov@comptel.com>
2018-01-05 17:21:29 +0200

    Solved first substring challenge

---
+++ src/main/java/hackerrank/strings/SubString.java
@@ -0,0 +1,19 @@
+package hackerrank.strings;
+
+import java.util.Scanner;
+
+/**
+ * https://www.hackerrank.com/challenges/java-substring/problem
+ * 
+ * @author cpt2tts
+ *
+ */
+public class SubString {
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		String S = in.next();
+		int start = in.nextInt();
+		int end = in.nextInt();
+		System.out.println(S.substring(start, end));
+	}
+}
