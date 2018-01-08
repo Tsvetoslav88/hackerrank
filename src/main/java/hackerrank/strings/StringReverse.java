bab8a268977a3d9f3f08539d1cb3d4262972b628
Tsvetoslav88 <tsvetoslav.tsvetkov@gmail.com>
2018-01-08 21:30:36 +0200

    Solved String Reverse challenge

---
+++ src/main/java/hackerrank/strings/StringReverse.java
@@ -0,0 +1,36 @@
+package hackerrank.strings;
+
+import java.util.Scanner;
+
+/**
+ * https://www.hackerrank.com/challenges/java-string-reverse/problem
+ * 
+ * @author TSVETOSLAV
+ *
+ */
+public class StringReverse {
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		String A = sc.next();
+		/* Enter your code here. Print output to STDOUT. */
+		sc.close();
+		String result = isPalindrome(A);
+		System.out.println(result);
+
+	}
+
+	private static String isPalindrome(String word) {
+
+		if (word.length() > 0 && word.length() < 50) {
+			for (int i = 0; i < word.length() / 2; i++) {
+				if (word.charAt(i) != word.charAt(word.length() - 1 - i)) {
+					return "No";
+				}
+			}
+
+		}
+		return "Yes";
+	}
+
+}
