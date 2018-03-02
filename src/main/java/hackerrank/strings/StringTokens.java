a607b210ebd512efb461078f28400546cc0d570b
Tsvetoslav88 <tsvetoslav.tsvetkov@gmail.com>
2018-03-02 21:33:04 +0200

    Solved String Tokens challenge

---
+++ src/main/java/hackerrank/strings/StringTokens.java
@@ -0,0 +1,33 @@
+package hackerrank.strings;
+
+import java.util.Scanner;
+
+/**
+ * https://www.hackerrank.com/challenges/java-string-tokens/problem
+ * 
+ * @author TSVETOSLAV
+ *
+ */
+public class StringTokens {
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		String s = "He is a very very good boy, isn't he?";//
+		// scan.nextLine();
+		// String s = "";
+
+		if (s.length() > 400000) {
+			return;
+		} else if (s == null || s.equals("") || s.trim().equals("")) {
+			System.out.println("0");
+		} else {
+			String[] splitStr = s.trim().split("[ !,?._'@]+");
+			System.out.println(splitStr.length);
+			for (String str : splitStr) {
+				System.out.println(str);
+			}
+		}
+
+		// Write your code here.
+		// scan.close();
+	}
+}
