b385ddcdba3fc3ebafc0c0e7585ef77ec9c4e7b1
Tsvetoslav <tsvetoslav.tsvetkov@gmail.com>
2018-06-19 11:04:28 +0300

    Added pattern syntax checker location

--- src/main/java/hackerrank/strings/PatternSyntaxChecker.java
+++ src/main/java/hackerrank/strings/PatternSyntaxChecker.java
@@ -3,6 +3,9 @@ package hackerrank.strings;
 import java.util.Scanner;
 import java.util.regex.Pattern;
 
+/**
+* https://www.hackerrank.com/challenges/pattern-syntax-checker/problem
+*/
 public class PatternSyntaxChecker {
 	public static void main(String[] args) {
 		Scanner in = new Scanner(System.in);
