6d36a66de3bc409f246bb0c012c58f3af2abdee7
Tsvetoslav <tsvetoslav.tsvetkov@gmail.com>
2018-07-06 12:02:35 +0300

    Solved Weather Observation Station 3 and 4 challenges

---
+++ src/main/java/sql/Weather Observation Station 3.sql
@@ -0,0 +1,9 @@
+--https://www.hackerrank.com/challenges/weather-observation-station-3/problem
+
+/**
+* Query a list of CITY names from STATION with even ID numbers only. You may print the results in any order, but must exclude duplicates from your answer.
+*/
+
+SELECT DISTINCT city
+FROM station
+WHERE MOD(id,2) = 0;
