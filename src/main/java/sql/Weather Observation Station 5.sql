137c76736a24586090acf051478fa4df5ded74f4
Tsvetoslav <tsvetoslav.tsvetkov@gmail.com>
2018-07-09 18:07:58 +0300

    Solved SQL Weather Observation Station 5 challenge

---
+++ src/main/java/sql/Weather Observation Station 5.sql
@@ -0,0 +1,20 @@
+--https://www.hackerrank.com/challenges/weather-observation-station-5/problem
+
+/**
+* Query the two cities in STATION with the shortest and longest CITY names, as well as their respective lengths (i.e.: number of characters in the name). 
+* If there is more than one smallest or largest city, choose the one that comes first when ordered alphabetically.
+*/
+
+SELECT city, city_length
+FROM(
+SELECT city, LENGTH(city) city_length
+FROM station
+ORDER BY LENGTH(city) ASC, city)
+WHERE ROWNUM = 1
+UNION ALL
+SELECT city, city_length
+FROM(
+SELECT city, LENGTH(city) city_length
+FROM station
+ORDER BY LENGTH(city) DESC, city)
+WHERE ROWNUM = 1;
