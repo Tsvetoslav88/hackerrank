68a140b9b3a655163a82ede02aee80be2a7c27cd
Tsvetoslav <tsvetoslav.tsvetkov@gmail.com>
2018-07-12 17:18:36 +0300

    Solved SQL Weather Observation Station 10

---
+++ src/main/java/sql/Weather Observation Station 10.sql
@@ -0,0 +1,9 @@
+--https://www.hackerrank.com/challenges/weather-observation-station-10/problem
+
+/**
+ * Query the list of CITY names from STATION that do not end with vowels. Your result cannot contain duplicates.
+*/
+
+SELECT DISTINCT city
+FROM STATION
+WHERE LOWER(SUBSTR(city,-1,1)) NOT IN ('a','e','i','o','u');
