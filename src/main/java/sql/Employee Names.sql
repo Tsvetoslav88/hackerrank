169b82415582d72bccf7d6c0480a7818eedd3729
Tsvetoslav <tsvetoslav.tsvetkov@gmail.com>
2018-07-16 17:10:14 +0300

    Solved 'Employee Names' challenge

---
+++ src/main/java/sql/Employee Names.sql
@@ -0,0 +1,9 @@
+--https://www.hackerrank.com/challenges/name-of-employees/problem?h_r=next-challenge&h_v=zen
+
+/**
+ * Write a query that prints a list of employee names (i.e.: the name attribute) from the Employee table in alphabetical order.
+ */
+
+SELECT name
+FROM Employee
+ORDER BY name;
