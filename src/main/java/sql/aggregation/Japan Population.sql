1ed98e996f55ab14cf3a37b0d4233f4d03d46092
Tsvetoslav <tsvetoslav.tsvetkov@gmail.com>
2018-07-20 16:36:05 +0300

    Solved SQL 'Japan Population'

---
+++ src/main/java/sql/aggregation/Japan Population.sql
@@ -0,0 +1,9 @@
+--https://www.hackerrank.com/challenges/japan-population/problem
+
+/**
+ * Query the sum of the populations for all Japanese cities in CITY. The COUNTRYCODE for Japan is JPN. 
+ */
+
+SELECT SUM(population)
+FROM city
+WHERE countrycode = 'JPN';
