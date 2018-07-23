79510c65b993162a1fad87d59bb0679c01f10e28
Tsvetoslav <tsvetoslav.tsvetkov@gmail.com>
2018-07-23 16:57:11 +0300

    Solved SQL 'Population Density Difference.sql' challenge

---
+++ src/main/java/sql/aggregation/Population Density Difference.sql
@@ -0,0 +1,8 @@
+--https://www.hackerrank.com/challenges/population-density-difference/problem
+
+/**
+ * Query the difference between the maximum and minimum populations in CITY. 
+ */
+
+SELECT MAX(population) - MIN(population)
+FROM city;
